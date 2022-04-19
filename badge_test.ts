f62075575c4d32fab23122f74d0223730facc4a2
Dong-Young Kim <31337.persona@gmail.com>
2022-04-20 00:57:47 +0900

    test: add badge tests

---
+++ badge_test.ts
@@ -0,0 +1,86 @@
+import { assertEquals } from "https://deno.land/std@0.135.0/testing/asserts.ts";
+import * as simpleIcons from "https://unpkg.com/simple-icons@6.19.0/icons.mjs";
+import { Badge, BadgeParameters, Schema } from "./badge.ts";
+import { colorForBackground } from "./color.ts";
+
+type BadgeKeys = keyof BadgeParameters;
+
+Deno.test("Deno Badge", async (t) => {
+  const { hex, slug } = simpleIcons.siDeno;
+
+  const given: BadgeParameters = {
+    label: "",
+    message: "Deno",
+  };
+  const expected: Schema = {
+    schemaVersion: 1,
+    label: "",
+    message: "Deno",
+    color: hex,
+    labelColor: colorForBackground(hex),
+    namedLogo: slug,
+    logoColor: hex,
+    style: "flat-square",
+  };
+
+  const actual = new Badge(given);
+
+  for (const key of Object.keys(expected) as BadgeKeys[]) {
+    await t.step(key, () => {
+      assertEquals(actual[key], expected[key]);
+    });
+  }
+});
+
+Deno.test("AWS Badge", async (t) => {
+  const { hex, slug } = simpleIcons.siAmazonaws;
+
+  const given: BadgeParameters = {
+    label: "",
+    message: "Amazon Web Services",
+    namedLogo: "amazon-aws",
+  };
+  const expected: Schema = {
+    schemaVersion: 1,
+    label: "",
+    message: "Amazon Web Services",
+    color: hex,
+    labelColor: colorForBackground(hex),
+    namedLogo: slug,
+    logoColor: hex,
+    style: "flat-square",
+  };
+
+  const actual = new Badge(given);
+
+  for (const key of Object.keys(expected) as BadgeKeys[]) {
+    await t.step(key, () => {
+      assertEquals(actual[key], expected[key]);
+    });
+  }
+});
+
+Deno.test("Volta Badge", async (t) => {
+  const given: BadgeParameters = {
+    label: "",
+    message: "Volta",
+  };
+  const expected: Schema = {
+    schemaVersion: 1,
+    label: "",
+    message: "Volta",
+    color: undefined,
+    labelColor: undefined,
+    namedLogo: undefined,
+    logoColor: undefined,
+    style: "flat-square",
+  };
+
+  const actual = new Badge(given);
+
+  for (const key of Object.keys(expected) as BadgeKeys[]) {
+    await t.step(key, () => {
+      assertEquals(actual[key], expected[key]);
+    });
+  }
+});
